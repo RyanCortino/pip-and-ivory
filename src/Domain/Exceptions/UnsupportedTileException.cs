22e989a6c032277d9c10052515f58e56be7d140f
Ryan Cortino <ryancortino@outlook.com>
2026-08-25 08:11:32 -0500

    feat: created tile value object

---
+++ src/Domain/Exceptions/UnsupportedTileException.cs
@@ -0,0 +1,7 @@
+﻿namespace PipAndIvory.Domain.Exceptions;
+
+public class UnsupportedTileException : Exception
+{
+    public UnsupportedTileException(string name)
+        : base($"Tile \"{name}\" is unsupported.") { }
+}
