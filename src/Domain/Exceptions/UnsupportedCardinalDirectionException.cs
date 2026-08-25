712c8c2627a98262cd45cb74998bca99936cfb3d
Ryan Cortino <ryancortino@outlook.com>
2026-08-25 08:11:57 -0500

    feat: created direction value object

---
+++ src/Domain/Exceptions/UnsupportedCardinalDirectionException.cs
@@ -0,0 +1,7 @@
+﻿namespace PipAndIvory.Domain.Exceptions;
+
+public class UnsupportedCardinalDirectionException : Exception
+{
+    public UnsupportedCardinalDirectionException(string code)
+        : base($"Cardinal direction \"{code}\" is unsupported.") { }
+}
