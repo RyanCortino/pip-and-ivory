fb761cac3510a5f3c48c7774d57fd0ee1ecf4bc6
Ryan Cortino <ryancortino@outlook.com>
2026-08-31 08:47:21 -0500

    feat: adds usecase for deleting a player

---
+++ tests/Application.FunctionalTests/Players/Commands/DeletePlayerTests.cs
@@ -0,0 +1,30 @@
+﻿using PipAndIvory.Application.Players.Commands.CreatePlayer;
+using PipAndIvory.Application.Players.Commands.DeletePlayer;
+using PipAndIvory.Domain.Entities;
+using PipAndIvory.Domain.ValueObjects.ReferenceTypes;
+
+namespace PipAndIvory.Application.FunctionalTests.Players.Commands;
+
+public class DeletePlayerTests : TestBase
+{
+    [Test]
+    public async Task ShouldRequireValidPlayerId()
+    {
+        var command = new DeletePlayerCommand(default(PlayerId));
+        await Should.ThrowAsync<NotFoundException>(() => TestApp.SendAsync(command));
+    }
+
+    [Test]
+    public async Task ShouldDeletePlayer()
+    {
+        var playerId = await TestApp.SendAsync(
+            new CreatePlayerCommand { DisplayName = "Test Player" }
+        );
+
+        await TestApp.SendAsync(new DeletePlayerCommand(playerId));
+
+        var player = await TestApp.FindAsync<Player>(playerId);
+
+        player.ShouldBeNull();
+    }
+}
