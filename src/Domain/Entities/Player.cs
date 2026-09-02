1b7ac1e4acd3f515847d0a6926cf2087868131de
Ryan Cortino <ryancortino@outlook.com>
2026-09-02 08:45:36 -0500

    feat: adds feature to record game history

--- src/Domain/Entities/Player.cs
+++ src/Domain/Entities/Player.cs
@@ -9,4 +9,15 @@ public class Player : BaseAuditableEntity<PlayerId>
     public GameStatistics BlockGameStats { get; private set; } = GameStatistics.None;
 
     public GameStatistics DrawGameStats { get; private set; } = GameStatistics.None;
+
+    public void RecordGameResult(GameVariant gamemode, bool won, int score)
+    {
+        if (gamemode != GameVariant.Block)
+        {
+            DrawGameStats = DrawGameStats.RecordGame(won, score);
+            return;
+        }
+
+        BlockGameStats = BlockGameStats.RecordGame(won, score);
+    }
 }
